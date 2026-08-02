44a978a3a47385d0c56bf475c8e91a4d8299ca8b
songdaramji <songdaramji@gmail.com>
2026-08-02 18:25:38 +0900

    Use backend for Kakao token exchange

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -7,12 +7,7 @@ const rest_api_key =
     : import.meta.env.VITE_KAKAO_REST_API_KEY;
 const appUrl = (import.meta.env.VITE_APP_URL || window.location.origin).replace(/\/$/, "");
 const redirect_uri = `${appUrl}/member/kakao`;
-const client_secret =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_KAKAO_CLIENT_SECRET_LOCAL
-    : import.meta.env.VITE_KAKAO_CLIENT_SECRET;
 const auth_code_path = "https://kauth.kakao.com/oauth/authorize";
-const access_token_url = "https://kauth.kakao.com/oauth/token";
 
 export const getKakaoLoginLink = () => {
   const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
@@ -20,22 +15,10 @@ export const getKakaoLoginLink = () => {
 };
 
 export const getToken = async (authCode) => {
-  const header = {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  };
-
-  // URLSearchParams를 사용하여 파라미터를 form-data 형식으로 변환
-  const params = new URLSearchParams();
-  params.append("grant_type", "authorization_code");
-  params.append("client_id", rest_api_key);
-  params.append("client_secret", client_secret); // client_secret 추가
-  params.append("redirect_uri", redirect_uri);
-  params.append("code", authCode);
-
-  const res = await axios.post(access_token_url, params, header);
-  return res.data; // 두 토큰 반환
+  const res = await axios.post(`${API_SERVER_HOST}/api/member/kakao/token`, null, {
+    params: { code: authCode, redirectUri: redirect_uri },
+  });
+  return res.data;
 };
 
 export const getMemberWithToken = async (accessToken, refreshToken) => {
