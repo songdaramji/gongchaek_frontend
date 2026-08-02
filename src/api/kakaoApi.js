0a599c59369bff349c431d45e4e783ba4c99a058
songdaramji <songdaramji@gmail.com>
2026-08-02 18:41:24 +0900

    Always show Kakao login prompt

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -10,7 +10,7 @@ const redirect_uri = `${appUrl}/member/kakao`;
 const auth_code_path = "https://kauth.kakao.com/oauth/authorize";
 
 export const getKakaoLoginLink = () => {
-  const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code&prompt=login`;
   return kakaoURL;
 };
 
